3793ca5f6b346ca6cdadd52ec8d5925e7d4b69a0
Masih <56260232+Masihbr@users.noreply.github.com>
2023-10-27 16:38:09 +0330

    Feature/add accounts app (#3)
    * Create accounts app
    
    * Add user model
    
    * Add djangorestframework to the project
    
    * Include accounts urls in _base.urls
    
    * Add signup functionality
    
    * Set SessionAuthentication as the default auth class
    
    * Add login functionality
    
    * Add logout functionality
    
    * Add profile view to edit info

--- _base/settings.py
+++ _base/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # 3rd party apps
+    'rest_framework',
+    
+    # local apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+# Django REST Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('signup/', views.UserSignUpAPIView.as_view(), name='sign-up'),
+    path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path("profile/", views.UserProfileRetrieveUpdateAPIView.as_view(), name="profile"),
+]
