e4386caf808993e92d42829a373030e54fc858c8
masihbr <masihbr@gmail.com>
2023-10-27 15:56:06 +0330

    Add user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from accounts import models as account_models
 
-# Register your models here.
+
+@admin.register(account_models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_verified', 'student_id', 'is_active']
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    student_id = models.CharField(max_length=9, validators=[RegexValidator(r'^\d{8,9}$')])
+    is_verified = models.BooleanField(default=False)
