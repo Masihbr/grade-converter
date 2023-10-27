22cc81d370c5f60b1748c34db7cdbf7044110c64
masihbr <masihbr@gmail.com>
2023-10-27 16:21:24 +0330

    Set SessionAuthentication as the default auth class

--- _base/settings.py
+++ _base/settings.py
@@ -129,3 +129,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# Django REST Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
